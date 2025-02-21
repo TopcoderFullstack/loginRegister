6d1feac1c28a77af874be1d8475ce02332e3670e
Topcoder Fullstack <124018604+topcoderguan@users.noreply.github.com>
2025-02-21 20:03:37 +1100

    update authToken

--- src/utils/authToken.js
+++ src/utils/authToken.js
@@ -3,15 +3,21 @@ import "dotenv/config"
 
 function authToken(req, res, next) {
   try {
-    const autnHeader = req.headers.authorization
-    if (!autnHeader) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
       console.log("没有token，可能是首次登录，直接继续");
       return next()
     }
 
-    const token = autnHeader.split(" ")[1]
+    const token = authHeader.split(" ")[1]
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
       if (err) {
+        if (err.name === 'TokenExpiredError') {
+          return res.status(401).json({
+            message: "Token has expired",
+            code: 6
+          })
+        }
         return res.status(403).json({
           message: "Invalid token",
           code: 2
